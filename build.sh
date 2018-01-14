c3cc6332bde849056f18a26ad849b703975a0466
Dridi Walid <wdridi1@gmail.com>
2018-01-14 16:45:39 +0100

    Fix security problem - append phing output

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@
 cd workspace && rm -rf ${2}
 git clone ${1} ${2} && cd $(basename $_ .git)
 git fetch && git checkout ${2}
-composer install && ./vendor/bin/phing >> ../${2}.log
+composer install && ./vendor/bin/phing > ../${2}.log
